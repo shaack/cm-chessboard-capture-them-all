9c39ace2b13acd5fa65e2a239dc2ef699617c709
shaack <shaack@gmail.com>
2025-05-22 20:50:22 +0200

    levelSelect page

--- src/Game.js
+++ src/Game.js
@@ -8,8 +8,25 @@ import {Markers} from "cm-chessboard/src/extensions/markers/Markers.js"
 import {Level} from "./Level.js"
 import {GameState} from "./GameState.js"
 
+
+export const LEVELS = {
+    "Rook": [
+        "8/8/8/8/2P1rP2/8/8/8 b - - 0 1",
+        "8/1P6/8/1P1r4/8/1P2P3/8/8 b - - 0 1",
+        "8/8/4P3/8/8/4P2P/1r2P3/P3P2P b - - 0 1",
+        "8/1P5P/8/P3P3/8/1P1P1P2/P6P/1P1r4 b - - 0 1",
+        "8/8/PP3P2/8/Pr3P2/3P4/P4P2/1P1P4 b - - 0 1",
+    ],
+    "Bishop": [ // Bishop
+        "8/8/4P3/1P1b4/2P5/3P4/8/8 b - - 0 1",
+        "4P1P1/8/P1P1P3/8/P3b3/7P/P3P3/3P1P2 b - - 0 1",
+        "3P1P2/2P5/1P5P/P5P1/3b4/4P1P1/8/4P3 b - - 0 1",
+        "6P1/1P5P/P7/3P4/4b3/3P3P/P5P1/1P3P2 b - - 0 1",
+    ]
+}
+
 export const LEVEL_FENS = [
-    
+
     [ // Rook
         "8/8/8/8/2P1rP2/8/8/8 b - - 0 1",
         "8/1P6/8/1P1r4/8/1P2P3/8/8 b - - 0 1",
@@ -30,8 +47,8 @@ export const LEVEL_FENS = [
         "8/3P4/5P2/2P1P3/4P1P1/3P2n1/5P2/7P b - - 0 1",
         "8/8/8/8/3nPPP1/3P2P1/3PPP1P/5P2 b - - 0 1",
         "1P4P1/3PPP2/1P1P3P/3PkP2/1P2P3/3P1P2/3P4/4P3 b - - 0 1",
-        
-        
+
+
     ],
     [ // Queen
         "4P3/1q6/2P3P2/P6P/8/5P1P/7P/1P2P3 b - - 0 1",
@@ -49,7 +66,7 @@ export class Game {
         this.restartButton = document.getElementById("restartButton")
         this.menuButton = document.getElementById("menuButton")
         this.closebtn = document.getElementById("closebtn")
-        this.restartButton.addEventListener("click", this.restartLevel.bind(this))   
+        this.restartButton.addEventListener("click", this.restartLevel.bind(this))
         this.menuButton.addEventListener("click", this.openNav.bind(this))
         this.closebtn.addEventListener("click", this.closeNav.bind(this))
         this.chessboard = new Chessboard(document.querySelector(".board"), {
@@ -106,7 +123,7 @@ export class Game {
     openNav() {
         document.getElementById("myNav").style.display = "block";
     }
-    
+
     closeNav() {
         document.getElementById("myNav").style.display = "none";
     }
